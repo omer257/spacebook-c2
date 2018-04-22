e3a450bfa4180b5e83fbe0e94b1330e06dc9ee6d
chaimochs <chaimochs@gmail.com>
2018-04-22 22:55:50 +0300

    Added CommentsSection

--- main.js
+++ main.js
@@ -7,34 +7,46 @@ const getTime = () => {
 }
 
 var addPost = (string, num) => {
-   var post = {text: string,
-               id: num
-                };
+    var post = {
+        text: string,
+        id: num
+    };
     posts.push(post);
 };
 
 var renderPost = () => {
     $(".posts").empty();
-    for(var i = 0; i < posts.length; i++){
-    $(".posts").append("<p " + "class = 'post' data-id = " + posts[i].id + "> " + posts[i].text + " " + "<button type = 'button' class = 'remove' >REMOVE</button></p>");
+    for (var i = 0; i < posts.length; i++) {
+        $(".posts").append("<p " + "class = 'post' data-id = " + posts[i].id + "> " + posts[i].text + " " + "<button type = 'button' class = 'btn btn-danger remove' >REMOVE</button> <input type='text' id= 'data-user' class = 'form-control' placeholder = 'Username'> <input type='text' id= 'data-comment' class = 'form-control' placeholder = 'Comments'> <button type = 'button' class = 'btn btn-success add-comment' >SEND</button></p>");
     }
 }
 
-$('.add-post').click(function(){
-   var postInput = $('#post-name').val();
-  var postNum = getTime();
+$('.add-post').click(function () {
+    var postInput = $('#post-name').val();
+    var postNum = getTime();
     addPost(postInput, postNum);
     renderPost();
 });
 
-$(".posts").on('click', '.remove', function(){ 
-    var clickedId = $(this).parent().data().id;
-    for(i = 0; i < posts.length; i++){
-        if(posts[i].id === clickedId){
-        posts.splice(i, 1);
+$(".posts").on('click', '.add-comment', function () {
+    var sentId = $(this).parent().data().id;
+    var sentUser = $(this).parent().find('#data-user').val();
+    var sentComment = $(this).parent().find('#data-comment').val();
+    for (i = 0; i < posts.length; i++) {
+        if (posts[i].id === sentId) {
+            posts[i].username = sentUser;
+            posts[i].comment = sentComment;
+        }
     }
-}
-    renderPost();
+    $(".posts").append("<p>" + sentUser + " says: " + sentComment + "</p>");
 });
 
-
+$(".posts").on('click', '.remove', function () {
+    var clickedId = $(this).parent().data().id;
+    for (i = 0; i < posts.length; i++) {
+        if (posts[i].id === clickedId) {
+            posts.splice(i, 1);
+        }
+    }
+    renderPost();
+});
